13c7a676c4ffbbff58438415027ccf0b8db8c19b
Johnathan Cao <jhcao@live.com>
2017-09-23 15:55:38 -0500

    Added new /addons endpoint for p5 models; db not yet functional

---
+++ server/db/index.js
@@ -0,0 +1,32 @@
+const { Pool } = require('pg')
+
+var config = {
+  host: 'localhost',
+  user: 'screamer',
+  password: 'screamer',
+  database: 'scream'
+};
+
+const pool = new Pool(config)
+
+module.exports = {
+  query: function (text, params, callback) {
+    // console.log('db.query, text: ', text);
+    // console.log('db.query, params: ', params);
+
+
+    return pool.query("select * from users")
+      .then(function(result) {
+        return result.rows;
+      });
+  },
+
+  qTest: function() {
+    return 'text from db file';
+  },
+
+  loggerTest: function(req, res, next) {
+    console.log('hello world from my db indexjs loggerTest middleware!');
+    next();
+  }
+}
