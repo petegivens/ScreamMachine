ad6affd12da41fae12ba9c672fb2d238fc58b9a5
Peter Choi <phc284@gmail.com>
2017-10-03 16:42:58 -0500

    working login with material ui, need to fix closing modal

---
+++ client/src/RoundEnd.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
+
+class RoundEnd extends React.Component{
+
+  constructor() {
+    state = {
+      open: false
+    }
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleRequestClose = () => {
+    this.setState({ open: false });
+  };
+
+
+  render () {
+    <div>
+      <Dialog open={this.state.open} onRequestClose={this.handleRequestClose}>
+        <DialogTitle>{"You've Passed Level ***Insert Level***"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Have You Screamed Enough?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleRequestClose} color="primary">
+              I'm Finished
+            </Button>
+            <Button onClick={this.handleRequestClose} color="primary">
+              Next Level
+            </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+
+  }
+
+
+}
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import {Modal, Button} from 'react-bootstrap';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
 
 const Login = (props) => (
   <div className="static-modal">
-		<Modal show={props.showLogin}>
-	  	<Modal.Header>
-	    	<Modal.Title>Login</Modal.Title>
-	  	</Modal.Header>
-	  	<Modal.Body>
-		    <label>Username:</label>
-		    <input placeholder='username' id='username'></input><br/>
-		    <label>Password:</label>
-		    <input placeholder='password' id='password' type='password'></input>
-	  	</Modal.Body>
-	  	<Modal.Footer>
-		    <Button onClick={props.closeModal}>Close</Button>
-		    <Button bsStyle="primary" onClick={props.login}>Sign In</Button>
-	  	</Modal.Footer>
-		</Modal>
+    <Dialog show={props.showLogin} onRequestClose={props.closeModal}>
+      <DialogTitle>
+        Login
+      </DialogTitle>
+      <DialogContent>
+        <form onSubmit={props.login}>
+          <TextField
+            id="username"
+          /><br />
+          <TextField
+            type="password"
+            id="password"
+          /><br />
+        </form>
+      </DialogContent>
+      <DialogActions>
+      <Button onClick={props.closeModal} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={props.login} color="primary">
+          Login
+        </Button>
+      </DialogActions>
+    </Dialog>
   </div>
 )
 
