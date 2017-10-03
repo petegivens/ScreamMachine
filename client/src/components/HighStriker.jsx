8da99aa1dd018f4386c88b0f66bf1bce65fc9dea
Ross Waycaster <rosswaycaster@me.com>
2017-10-03 16:55:58 -0500

    changes

---
+++ client/src/components/HighStriker.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HighStriker extends React.Component {
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default HighStriker;
