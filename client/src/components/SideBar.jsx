137e288c89d3f33c03031e1d186aba2cdcb8d917
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-10-04 16:02:49 -0500

    added state to SideBar

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -2,23 +2,33 @@ import React from 'react';
 import Card, {CardContent} from 'material-ui/Card';
 import Grid from 'material-ui/Grid';
 
-const SideBar = () => (
+class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-    <div>
-      <Grid container spacing={24}>
-        <Grid item xs={12} sm={6}>
-          <Card>
-            <CardContent>This is your Highest Score!!!!!</CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Card>
-            <CardContent>These are Other Peoples High Scores!!!!</CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </div>
+    }
+  }
 
-);
+
+  render() {
+    return(
+      <div>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={6}>
+            <Card>
+              <CardContent>This is your Highest Score!!!!!</CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Card>
+              <CardContent>These are Other Peoples High Scores!!!!</CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+    )
+  };
+};
 
 export default SideBar;
