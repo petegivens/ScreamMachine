10e1a143b3aa7f34aa72850123ba9452b342b36c
Peter Choi <phc284@gmail.com>
2017-10-04 15:56:31 -0500

    rebase

--- client/src/components/Arcade.jsx
+++ client/src/components/Arcade.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import HighStriker from './HighStriker.jsx';
+import LevelEnd from './LevelEnd.jsx'
 import SideBar from './SideBar.jsx';
-
 class Arcade extends React.Component {
 
   render() {
     return (
       <div>
+      <div>
         <HighStriker />
         <SideBar />
+      </div>
+      <div>
+        <LevelEnd />
+      </div>
       </div>
     );
   }
--- client/src/components/LevelEnd.jsx
+++ client/src/components/LevelEnd.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React from 'react';
 import Dialog, {
   DialogActions,
   DialogContent,
   DialogContentText,
-  DialogTitle,
+  DialogTitle
 } from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
+import {
+  ShareButtons,
+  ShareCounts,
+  generateShareIcon
+} from 'react-share';
 
-export default class LevelEnd extends React.Component{
+const {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  TwitterShareButton
+} = ShareButtons;
 
+<<<<<<< HEAD
   constructor() {
     state = {
+=======
+const FacebookIcon = generateShareIcon('facebook');
+const TwitterIcon = generateShareIcon('twitter');
+const GooglePlusIcon = generateShareIcon('google');
+
+
+export default class LevelEnd extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+>>>>>>> rebase
       open: false
     }
-  }
 
-  handleClickOpen = () => {
-    this.setState({ open: true });
+    this.handleOpen = this.handleOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+    this.handleNextLevel = this.handleNextLevel.bind(this)
   };
 
+<<<<<<< HEAD
   handleRequestClose = () => {
     this.setState({ open: false });
   };
@@ -42,8 +66,63 @@ export default class LevelEnd extends React.Component{
         </DialogActions>
       </Dialog>
     </div>
-
+=======
+  handleOpen() {
+    this.setState({
+      open: true
+    })
+    console.log(window.location)
   }
 
+  handleClose() {
+    this.setState({
+      open: false
+    })
+  }
 
+  handleNextLevel() {
+    //change the state of HighStriker to go up to the next level
+  }
+>>>>>>> rebase
+
+  handleStartOver() {
+    //change the state of HighStriker to go back to the first level
+  }
+
+  render () {
+    return (
+      const url = "https://www.youtube.com/watch?v=J---aiyznGQ";
+      const quote = 'I Got to Level **insert level**. Think You Can Beat Me?'
+
+      <div>
+        <Button raised onClick={this.handleOpen}>LevelEnd</Button>
+          <Dialog open={this.state.open} onRequestClose={this.handleClose}>
+            <DialogTitle>{"You've Passed Level ***Insert Level***"}</DialogTitle>
+            <DialogContent>
+              <div>
+                <FacebookShareButton
+                  url={url}
+                  quote={quote}>
+                <FacebookIcon
+                  size={32}
+                  square />
+                </FacebookShareButton>
+              </div>
+              <DialogContentText>
+                Have You Screamed Enough?
+              </DialogContentText>
+
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                  I'm Finished
+                </Button>
+                <Button onClick={this.handleClose} color="primary">
+                  Next Level
+                </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+    )
+  }
 }
