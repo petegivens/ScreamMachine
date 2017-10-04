c3688cbe7cae32437d16b3bf75f20289d2ee2913
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-10-04 11:05:01 -0500

    adding sidebar

---
+++ client/src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from 'material-ui/Dialog';
+
+export default class SideBar extends React.Component{
+
+
+  render () {
+    <div>
+      <Card></Card>
+    </div>
+
+  }
+}
