2f8f58948974ea86a31696b400f6b5414c58aebf
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-10-04 15:55:50 -0500

    cards in high score feature side by side

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -5,15 +5,17 @@ import Grid from 'material-ui/Grid';
 const SideBar = () => (
 
     <div>
-      <Grid item xs={12} sm={6}>
-        <Card>
-          <CardContent>Sup</CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <Card>
-          <CardContent>Hey</CardContent>
-        </Card>
+      <Grid container spacing={24}>
+        <Grid item xs={12} sm={6}>
+          <Card>
+            <CardContent>This is your Highest Score!!!!!</CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Card>
+            <CardContent>These are Other Peoples High Scores!!!!</CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </div>
 
