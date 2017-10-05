f2c2ad1eca6afee9f0f0d7b609a292784e5796dd
Ross Waycaster <rosswaycaster@me.com>
2017-10-05 10:48:05 -0500

    add margin top to page

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -166,7 +166,9 @@ class App extends React.Component {
       <div>
         <NavBar isLoggedIn={this.state.isLoggedIn} showLegacy={this.showLegacy} page={this.state.page}/>
         <Signup closeModal={this.closeModal} showSignup={this.state.showSignup} signup={this.signup}/>
-        {page}
+        <div style={{marginTop:65}}>
+          {page}
+        </div>
       </div>
     )
   }
