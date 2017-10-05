f86b014c7f542d114bb6f6a158e34b0bc4a9c534
Pete Givens <pgivens@gmail.com>
2017-10-05 00:00:43 -0500

    changed from a button to a switch and moved it into a separate component in LegacySwitch.jsx

--- client/src/components/LegacySwitch.jsx
+++ client/src/components/LegacySwitch.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import Switch from 'material-ui/Switch';
 import PropTypes from 'prop-types';
-import { createStyleSheet, withStyles } from 'material-ui/styles';
 import { FormControlLabel } from 'material-ui/Form';
 import green from 'material-ui/colors/green';
-
-const styles = createStyleSheet("LegacySwitch", {
-  bar: {},
-  checked: {
-    color: green[500],
-    '& + $bar': {
-      backgroundColor: green[500],
-    },
-  }
-})
 
 export default class LegacySwitch extends React.Component {
   constructor(props) {
@@ -31,9 +20,6 @@ export default class LegacySwitch extends React.Component {
       <FormControlLabel
         control={
           <Switch
-            classes={{
-              checked: classes.checked
-            }}
             checked={this.state.showLegacy}
             onChange={ () => {
               this.props.showLegacy();
