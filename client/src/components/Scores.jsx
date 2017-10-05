70bc7a522acc9cb5a8a0424fb3fe7e2e34cec04c
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-10-05 15:36:35 -0500

    added font and chips

--- client/src/components/Scores.jsx
+++ client/src/components/Scores.jsx
@@ -6,6 +6,7 @@ import Typography from 'material-ui/Typography';
 import Chip from 'material-ui/Chip';
 import { withStyles, withTheme } from 'material-ui/styles';
 
+
 const styles = {
   card: {
     textAlign: 'center',
