0764fc14c45e8479ee70baf977ce9fc47b143356
Pete Givens <pgivens@gmail.com>
2017-10-05 16:04:47 -0500

    removed duplicate import

--- client/src/components/Scores.jsx
+++ client/src/components/Scores.jsx
@@ -5,7 +5,6 @@ import { withStyles, withTheme } from 'material-ui/styles';
 import HighScores from './HighScores.jsx';
 import Typography from 'material-ui/Typography';
 import Chip from 'material-ui/Chip';
-import { withStyles, withTheme } from 'material-ui/styles';
 
 const styles = {
   card: {
