cdd6bc69f97bfb5e3ae9022a8a267b938f984f8e
Ross Waycaster <rosswaycaster@me.com>
2017-10-05 15:28:44 -0500

    materialize HighStriker

--- client/src/components/HighStriker.jsx
+++ client/src/components/HighStriker.jsx
@@ -26,7 +26,9 @@ const style = {
     paddingTop: 20,
     marginBottom: 20,
     position: 'relative',
-    height: 625
+    height: 625,
+    borderRadius: 2,
+    boxShadow: '0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)'
   },
   machine: {
     position: 'relative',
