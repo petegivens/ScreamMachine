0e5157d1c155cdee550f674f6f506e4fc6738685
Ross Waycaster <rosswaycaster@me.com>
2017-10-04 19:24:01 -0500

    set dialog based on pass or fail

--- client/src/components/LevelEnd.jsx
+++ client/src/components/LevelEnd.jsx
@@ -28,7 +28,8 @@ export default class LevelEnd extends React.Component {
     super(props)
 
     this.state = {
-      open: props.levelEnd || false
+      open: props.levelEnd || false,
+      payload: {}
     }
 
     this.handleOpen = this.handleOpen.bind(this)
@@ -67,59 +68,102 @@ export default class LevelEnd extends React.Component {
   render () {
     const url = "https://www.youtube.com/watch?v=J---aiyznGQ";
     const quote = 'I Got to Level **insert level**. Think You Can Beat Me?'
-    return (
 
-      <div>
-        <Dialog open={this.state.open} onRequestClose={this.handleClose}>
-          <DialogTitle>{"You've Passed Level ***Insert Level***"}</DialogTitle>
-          <DialogContent>
-            {JSON.stringify(this.state.payload)}
-            <div> Share Your Score! </div>
-            <div className="sharebutton">
-              <FacebookShareButton
-                url={url}
-                quote={quote}>
-                <FacebookIcon
-                  size={32}
-                square />
-              </FacebookShareButton>
-            </div>
-            <div className="sharebutton">
-              <TwitterShareButton
-                url={url}
-                quote={quote}>
-                <TwitterIcon
-                  size={32}
-                square />
-              </TwitterShareButton>
-            </div>
-            <div className="sharebutton">
-              <GooglePlusShareButton
-                url={url}
-                quote={quote}>
-                <GooglePlusIcon
-                  size={32}
-                square />
-              </GooglePlusShareButton>
-            </div>
-            <DialogContentText className="levelEndText">
-              Have You Screamed Enough?
-            </DialogContentText>
+    const passed = (
+      <Dialog open={this.state.open} onRequestClose={this.handleClose}>
+        <DialogTitle>{"You've Passed Level ***Insert Level***"}</DialogTitle>
+        <DialogContent>
+          {JSON.stringify(this.state.payload)}
+          <div> Share Your Score! </div>
+          <div className="sharebutton">
+            <FacebookShareButton
+              url={url}
+              quote={quote}>
+              <FacebookIcon
+                size={32}
+              square />
+            </FacebookShareButton>
+          </div>
+          <div className="sharebutton">
+            <TwitterShareButton
+              url={url}
+              quote={quote}>
+              <TwitterIcon
+                size={32}
+              square />
+            </TwitterShareButton>
+          </div>
+          <div className="sharebutton">
+            <GooglePlusShareButton
+              url={url}
+              quote={quote}>
+              <GooglePlusIcon
+                size={32}
+              square />
+            </GooglePlusShareButton>
+          </div>
 
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              I'm Finished
-            </Button>
-            <Button onClick={this.handleStartOver} color="primary">
-              Start Over
-            </Button>
-            <Button onClick={this.handleClose} color="primary">
-              Next Level
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose} color="primary">
+            I'm Finished
+          </Button>
+          <Button onClick={this.handleClose} color="primary">
+            Next Level
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+
+    const failed = (
+      <Dialog open={this.state.open} onRequestClose={this.handleClose}>
+        <DialogTitle>{"You didn't pass this level"}</DialogTitle>
+        <DialogContent>
+          {JSON.stringify(this.state.payload)}
+          <div> Share Your Score! </div>
+          <div className="sharebutton">
+            <FacebookShareButton
+              url={url}
+              quote={quote}>
+              <FacebookIcon
+                size={32}
+              square />
+            </FacebookShareButton>
+          </div>
+          <div className="sharebutton">
+            <TwitterShareButton
+              url={url}
+              quote={quote}>
+              <TwitterIcon
+                size={32}
+              square />
+            </TwitterShareButton>
+          </div>
+          <div className="sharebutton">
+            <GooglePlusShareButton
+              url={url}
+              quote={quote}>
+              <GooglePlusIcon
+                size={32}
+              square />
+            </GooglePlusShareButton>
+          </div>
+          <DialogContentText className="levelEndText">
+            Have You Screamed Enough?
+          </DialogContentText>
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose} color="primary">
+            I'm Finished
+          </Button>
+          <Button onClick={this.handleStartOver} color="primary">
+            Start Over
+          </Button>
+        </DialogActions>
+      </Dialog>
     )
+
+    return this.state.payload.score >= 100 ? passed : failed;
   }
 }
