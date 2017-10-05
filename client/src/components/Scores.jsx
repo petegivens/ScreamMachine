6360adb4b99472fe3a6816f18b8a86c6485e2b74
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-10-05 14:18:03 -0500

    rendering three cards

--- client/src/components/Scores.jsx
+++ client/src/components/Scores.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import Card, {CardHeader, CardContent} from 'material-ui/Card';
+import Card, {CardHeader, CardContent, CardMedia} from 'material-ui/Card';
 import Grid from 'material-ui/Grid';
 import HighScores from './HighScores.jsx'
 
 const style = {
   card: {
     backgroundColor: '#36bfc7',
-    textAlign: 'center',
+    textAlign: 'center'
   },
   cardContent: {
     fontSize: '36px'
+  },
+  img: {
+    width: '100%'
   }
 }
 
@@ -31,20 +34,35 @@ class Scores extends React.Component {
     return(
       <div>
         <Grid container spacing={24}>
-          <Grid item xs={12}>
+          <Grid item xs={12} sm={12}>
+          <Card >
+            <CardMedia>
+              <img src='../models/carnival_image.jpg' style={style.img}/>
+            </CardMedia>
+          </Card>
+          </Grid>
+          <Grid item xs={12} sm={12}>
             {this.props.user ?
               <Card style={style.card}>
-                <CardHeader title="Your HIGHEST Score!"/>
-                <CardContent>{this.props.user.personalBest}</CardContent>
+                <Grid container spacing={24}>
+                  <Grid item xs>
+                    <CardHeader title="Your HIGHEST Score!"/>
+                    <CardContent>{this.props.user.personalBest}</CardContent>
+                  </Grid>
+                  <Grid item xs>
+                    <CardHeader title="Your HIGHEST Score!"/>
+                    <CardContent>{this.props.currentScore}</CardContent>
+                  </Grid>
+                </Grid>
               </Card>
               :
               <Card style={style.card}>
-                <CardHeader title="Your HIGHEST Score!"/>
-                <CardContent style={style.cardContent}>1</CardContent>
+                <CardHeader title="Your Current Score!"/>
+                <CardContent style={style.cardContent}>{this.props.currentScore}</CardContent>
               </Card>
             }
           </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={12} sm={12}>
             <Card style={style.card}>
               <CardHeader title="Other Peoples High Score!"/>
               <CardContent>
