8bad6e8612a4a0c1e0fd4a68d732673cd98175f7
Peter Choi <phc284@gmail.com>
2017-10-06 10:24:43 -0500

    rebased

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -12,13 +12,13 @@ import Login from './Login.jsx';
 import Signup from './Signup.jsx';
 import NavProfile from './NavProfile.jsx';
 
-const NavBar = ({ showLegacy, login, logout, user, isLoggedIn, signup, page}) => {
+const NavBar = ({ showLegacy, login, logout, user, isLoggedIn, signup, page, navHandler}) => {
     const background = page === 'Arcade' ? 'primary' : 'default';
     return (
       <AppBar position="absolute" color={background}>
         <Toolbar>
           {(page === 'scream' || page === 'Profile' || page === 'StressForm') &&
-            <NavProfile />
+            <NavProfile navHandler={navHandler}/>
           }
           <div style={{ flex: 1 }}>
             <LegacySwitch showLegacy={showLegacy} page={page}/>
